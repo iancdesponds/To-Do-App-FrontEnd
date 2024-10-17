b0b0e41d70b2fa0a6aea5f20570d9557f17f02fb
iancdesponds <iancdesponds@gmail.com>
2024-10-17 11:55:14 -0300

    feat: url back-end

--- to-do-app/src/api.js
+++ to-do-app/src/api.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:8000';
+const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:8000';
 
 // Função para buscar todas as tarefas
 export const getTasks = async () => {
